1eb675c6426f7144b7d6dc4823abe24e41ecf77e
mztnnrt <remztnnrt@gmail.com>
2022-03-06 13:03:16 +0900

    add sample

---
+++ sample/backend/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "Hello World 🌎")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
