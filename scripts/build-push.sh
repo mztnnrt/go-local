d8e69ef6cb763c2ff85be05f1be62798718a2b00
mztnnrt <remztnnrt@gmail.com>
2022-08-22 17:52:25 +0900

    chore: go 1.19

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -4,5 +4,6 @@ docker build . -t mztnnrt/go-local:1.17 -f golang:1.17/Dockerfile &&\
 docker push mztnnrt/go-local:1.17 &&\
 docker build . -t mztnnrt/go-local:1.18 -f golang:1.18/Dockerfile &&\
 docker push mztnnrt/go-local:1.18
-docker build . -t mztnnrt/go-local:latest -f golang:1.18/Dockerfile &&\
+docker build . -t mztnnrt/go-local:1.19 -t mztnnrt/go-local:latest -f golang:1.19/Dockerfile &&\
+docker push mztnnrt/go-local:1.19
 docker push mztnnrt/go-local:latest
