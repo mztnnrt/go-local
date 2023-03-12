d927614c2ac7f640b6c9f2d5001ec2b1d4c46b7c
mztnnrt <remztnnrt@gmail.com>
2023-03-12 19:16:30 +0900

    chore: setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,5 @@
 apk upgrade &&\
 # build-base package is required delve
 apk --no-cache add curl build-base &&\
-curl -sSfL https://raw.githubusercontent.com/cosmtrek/air/master/install.sh | sh -s &&\
+go install github.com/cosmtrek/air@latest &&\
 go install github.com/go-delve/delve/cmd/dlv@latest
