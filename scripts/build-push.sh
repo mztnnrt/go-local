2877615567721364d8167c526c80d413b11561a9
mztnnrt <remztnnrt@gmail.com>
2023-07-09 12:52:37 +0900

    chore: go 1.21

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-# docker build . -t mztnnrt/go-local:1.17 -f golang:1.17/Dockerfile &&\
-# docker push mztnnrt/go-local:1.17 &&\
-
-# docker build . -t mztnnrt/go-local:1.18 -f golang:1.18/Dockerfile &&\
-# docker push mztnnrt/go-local:1.18
-
-# docker build . -t mztnnrt/go-local:1.19 -f golang:1.19/Dockerfile &&\
-# docker push mztnnrt/go-local:1.19
-
-docker build . -t mztnnrt/go-local:1.20 -t mztnnrt/go-local:latest -f golang:1.20/Dockerfile &&\
-docker push mztnnrt/go-local:1.20
+docker build . -t mztnnrt/go-local:1.21 -t mztnnrt/go-local:latest -f golang:1.21/Dockerfile &&\
+docker push mztnnrt/go-local:1.21
 docker push mztnnrt/go-local:latest
